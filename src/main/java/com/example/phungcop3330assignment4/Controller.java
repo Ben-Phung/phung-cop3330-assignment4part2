41d47320454dbece2d4d96785d9e72cdb9d58777
Ben Phung <benphung123@gmail.com>
2021-11-01 23:26:07 -0400

    Initial commit

---
+++ src/main/java/com/example/phungcop3330assignment4/Controller.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Ben Phung
+ */
+
+package com.example.phungcop3330assignment4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    protected void onAddTodoButtonClick() {
+        // When Add new task button is clicked switch to that scene
+
+    }
+
+    @FXML
+    protected void onEditTaskButtonClick() {
+        // When Edit task button is clicked call function
+    }
+
+    @FXML
+    protected void onMarkAsCompleteButtonClick() {
+        // When mark as complete button is clicked call function
+    }
+
+    @FXML
+    protected void onDeleteButtonClick() {
+        // When delete button is clicked call function
+    }
+
+}
---
+++ src/main/java/com/example/phungcop3330assignment4/TodoList.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Ben Phung
+ */
+
+package com.example.phungcop3330assignment4;
+
+public class TodoList {
+
+    public void addItem()
+    {
+        // If button is pressed
+        // Move to scene for add item
+    }
+    public void removeItem()
+    {
+        // If an item is selected as button is pressed
+        // Remove item
+    }
+    public String edit()
+    {
+        // Format of list will be Title | Description | Date | Status
+        // Each text will be editable when selected and button is pressed
+        // Returns new input
+        return "";
+    }
+    public boolean markComplete()
+    {
+        // boolean type to return if item is marked as complete
+        // Move completed item to Completed side of table
+        return false;
+    }
+    public void delete()
+    {
+        // Item selected will be deleted
+    }
+    public void save()
+    {
+        // Save current item
+    }
+    public void saveAll()
+    {
+        // Save all items
+    }
+}
